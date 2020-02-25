7d6054f3ead9a2915fe9aebab270f26549915dee
Brandon Winslow <bewinslo@ncsu.edu>
2020-02-25 10:28:24 -0500

    removed track modifications warning

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://admin:Password1!@localhost/backend')
 
 db = SQLAlchemy(app)
