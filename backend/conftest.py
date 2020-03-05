bfbe9d5db2c0c550629f734308a8834154b260e2
Mark Sommers <mssommer@ncsu.edu>
2020-03-05 10:21:21 -0500

    Added Database Resetting to UnitTests

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
-from app import app
+from app import app, db
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
+    db.drop_all()
+    db.create_all()
     with app.test_client() as client:
         yield client
