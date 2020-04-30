0192b7fe3164adbe90117aa87f4e29b5c70b2fde
Brandon Winslow <bewinslo@ncsu.edu>
2020-04-30 12:19:45 -0400

    added comments to view endpoints

--- backend/src/blueprints/views/routes.py
+++ backend/src/blueprints/views/routes.py
@@ -6,7 +6,7 @@ from pytz import timezone
 
 from app import db
 from src.algorithms.categorize import categorizeFromBuckets
-from src.blueprints.core.routes import get_cohorts
+from src.blueprints.core.routes import get_cohorts, create_job
 from src.blueprints.views.bp import bp
 from src.models.Cohort import Cohort
 from src.models.Communication import Communication
@@ -19,72 +19,47 @@ from src.models.QueueStatus import QueueStatus
 def index():
     return render_template('index.html')
 
-# @bp.route("/view/users")
-# def get_users_default():
-
-#     return get_users(0)
-
-# @bp.route("/view/users")
-# def get_users():
-
-#     comms = Communication.query.all()
-#     userList = []
-#     count = 0
-#     currentIdx = 0
-#     # smallList = []
-#     for com in comms:
-#         count = count + 1
-#         comDict = dict()
-#         comDict['ID'] = com.accountId
-#         comDict['date/time'] = com.notification_date_time
-#         comDict['method'] = com.method
-#         comDict['type'] = com.notification_type
-#         userList.append(comDict)
-
-#         if count == 20:
-#             # userList.append(smallList)
-#             # smallList = []
-#             # if currentIdx == idx:
-#             # for right now, only send the first 20 entries
-#             break
-
-#     return render_template('users.html', users=userList)
-
+# returns a form that allows patient demographics input
+# if a POST request, the inputted patient data and the corresponding cohort data are returned
 @bp.route('/view/user', methods=['POST', 'GET'])
 def get_cohort_by_user_stats():
-  if request.method == 'POST':
-      form = request.form
-      # print(form)
-      patientDict = dict()
-      patientDict['id'] = 0
-      # print(form['fullDateOfBirth'])
+    if request.method == 'POST':
 
-      patientDict['dateOfBirth'] = int(form['fullDateOfBirth'][:4])
-      if form['gender'] == "male":
-        patientDict['gender'] = 'M'
-      else:
-        patientDict['gender'] = 'F'
-      patientDict['income'] = int(form['income'])
-      patientDict['billAmount'] = int(form['billAmount'])
-      patientDict['OptOut'] = form['optOut']
+        # grab patient stats from form
+        form = request.form
+        patientDict = dict()
+        patientDict['id'] = 0
+        patientDict['dateOfBirth'] = int(form['fullDateOfBirth'][:4])
+        if form['gender'] == "male":
+            patientDict['gender'] = 'M'
+        else:
+            patientDict['gender'] = 'F'
+        patientDict['income'] = int(form['income'])
+        patientDict['billAmount'] = int(form['billAmount'])
+        patientDict['OptOut'] = form['optOut']
 
-      print(patientDict)
-      cID = str(categorizeFromBuckets(patientDict))
-      patientDict['paperID'] = cID[0]
-      patientDict['textID'] = cID[1]
-      patientDict['emailID'] = cID[2]
-      patientDict['fullDateOfBirth'] = form['fullDateOfBirth']
+        # find where this patient falls into a cohort
+        cID = str(categorizeFromBuckets(patientDict))
+        patientDict['paperID'] = cID[0]
+        patientDict['textID'] = cID[1]
+        patientDict['emailID'] = cID[2]
+        patientDict['fullDateOfBirth'] = form['fullDateOfBirth']
 
-      return render_template("user-form-single.html", cohort=patientDict)
-  return render_template("user-form.html", cohort="")
+        return render_template("user-form-single.html", cohort=patientDict)
 
+    return render_template("user-form.html")
+
+# returns a human-reable table of the current cohorts in the database (grouped by cohort ID)
 @bp.route("/view/cohorts")
 def get_cohorts_view():
-    c = Cohort.query.all()
-    # print(c)
+
+    # gather all cohort IDs (non-duplicates)
+    cohorts = Cohort.query.all()
     cohortSet = set()
-    for cohort in c:
+    for cohort in cohorts:
         cohortSet.add(cohort.cid)
+
+    # add each cohort ID to the list
     cohortList = []
     for cohortID in cohortSet:
         cDict = dict()
@@ -94,16 +69,17 @@ def get_cohorts_view():
         cDict['texts per cycle'] = cID[1]
         cDict['emails per cycle'] = cID[2]
         cohortList.append(cDict)
-    # print(result)
-    # cohortList = {'cohort ID' : cohortSet}
-    cohortList = sorted(cohortList, key=lambda k:k['cohort ID'])
-    # print(cohortList)
+
+    cohortList = sortList(cohortList, 'cohort ID')
+
     return render_template('cohorts.html', cohorts=cohortList)
 
+# returns the cohorts of a given cohort ID
 @bp.route("/view/cohorts/<int:cohortID>")
-def get_patients_by_cohort(cohortID):
-    c = Cohort.query.filter_by(cid=cohortID).all()
+def get_by_cohort_id(cohortID):
     cohortList = []
+
+    c = Cohort.query.filter_by(cid=cohortID).all()
     for cohort in c:
         cDict = dict()
         # cDict['cohort ID'] = cohort.cid
@@ -118,26 +94,37 @@ def get_patients_by_cohort(cohortID):
 
     return render_template('cohorts-single.html', cohortID=cohortID, cohorts=cohortList)
 
-# Retrieve a list of jobs currently in the job queue database
-@bp.route("/view/jobs")
+# returns a user-readble table of the current jobs in the database
+# if a POST request is made, the job is created with the specified algorithm
+@bp.route("/view/jobs", methods=['POST', 'GET'])
 def get_jobs():
-    jobs = QueueJob.query.all()
+    if request.method == 'POST':
+        create_job(request.form['algorithm'])
+
     jobListUnsorted = []
+
+    jobs = QueueJob.query.all()
     for job in jobs:
         jobDict = dict()
         jobDict['job ID'] = job.id
         jobDict['status'] = get_status(job.status)
-        jobDict['job algorithm'] = job.algorithm
+        jobDict['job algorithm'] = job.algorithm.replace('_', ' ')
         jobDict['date created'] = get_pretty_date(job.date_created)
         jobListUnsorted.append(jobDict)
 
-    jobList = sorted(jobListUnsorted, key=lambda k:k['job ID'])
+    jobList = sortList(jobListUnsorted, 'job ID')
 
     return render_template('jobs.html', jobs=jobList)
 
+# returns a user-readable queue status
 def get_status(key):
     return QueueStatus(key).name.replace('_', ' ')
 
+# returns a pretty date converted from UTC to US/Eastern
 def get_pretty_date(oldDate):
     newDate = timezone('US/Eastern').fromutc(oldDate)
     return newDate.strftime("%d %b %Y at %H:%M")
+
+# returns a list of dictionaries (unsortedList) sorted by a key (key)
+def sortList(unsortedList, key):
+    return sorted(unsortedList, key=lambda k:k[key])
